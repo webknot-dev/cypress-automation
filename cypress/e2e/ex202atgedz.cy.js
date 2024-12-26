a0f8d9d065f5bb811a1bf4c361f097e24d8739c5
noordeen123 <noordeen.cse_c2019@crescent.education>
2024-12-26 15:12:58 +0530

    Add EX202A automation test and update fixture data for transfer goods process

---
+++ cypress/e2e/ex202atgedz.cy.js
@@ -0,0 +1,50 @@
+describe("Automating EX202A Transfer Goods for Export From a Designated Zone", () => {
+    before(() => {
+        // Set the viewport size for the tests
+        cy.viewport(1024, 764)
+    })
+    it("EX202A - TGEDZ", () => {
+        // Load data from the fixture file
+        cy.fixture("example.json").then((data) => {
+            // Visit the URL specified in the fixture data
+            cy.visitUrl(data.EntryValues.url)
+            cy.log("Navigated to FTA - Federal Tax Authority Successfully");
+
+            // Perform login using the credentials and locators from the fixture data
+            cy.login(data.LocatorsPath.emailInputField_id,
+                data.AuthDetails.email,
+                data.LocatorsPath.passwordInputField_id,
+                data.AuthDetails.password,
+                data.LocatorsPath.captchaInputField_id,
+                data.LocatorsPath.captcha_element_id,
+                data.LocatorsPath.loginButton_id,
+                "ex202atgedz.cy.js"
+            )
+
+            // Click on the taxable profile button
+            cy.clickXpathElement(data.LocatorsPath.taxableProfileButton_xpath)
+            // Close any popup if present
+            cy.closePopupIfPresent(data.LocatorsPath.PopUp_Selector, data.LocatorsPath.PopUp_Accept)
+            // Wait for the TRN validation element to be present
+            cy.waitForXpathElementPresence(data.LocatorsPath.TRN_validation_Xpath)
+            // Click on the excise tax button
+            cy.clickXpathElement(data.LocatorsPath.exciseTax_xpath);
+
+            // Select the excise option using the locators and values from the fixture data
+            cy.selectExcise(data.locators_ex202atgedz.EX202ATGEDZ_container_id,
+                data.locators_ex202atgedz.EX202ATGEDZ_title_id,
+                data.locators_ex202atgedz.EX202ATGEDZ_description_id,
+                data.locators_ex.EX_createNew_id,
+                "EX202A",
+                data.EntryValues.EX202ATGEDZ_Description
+            )
+
+            // Click on the checkbox to agree to terms
+            cy.clickElement(data.locators_ex.EX_checkbox_id)
+            // Click on the start button to begin the process
+            cy.clickXpathElement(data.locators_ex.EX_Start_xpath)
+            // Input the designated zone number
+            cy.inputField(data.locators_ex.EX_zone_id2, data.EntryValues.EX202ATGEDZ_zone_number)
+        })
+    });
+})
