1dd14cbece205842c23427aba4eb10337cbb76df
skharipriya <153832106+skharipriya@users.noreply.github.com>
2025-01-17 17:42:17 +0530

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -257,27 +257,28 @@ Cypress.Commands.add('validateTRNandName', (trn, name) => {
 });
 
 // Custom command for file upload for images
+// Custom command for file upload for images
 Cypress.Commands.add('uploadFile', (addButtonSelector, fileName, okButtonSelector1, okButtonSelector2) => {
     // Click the "Add" button
     cy.get(addButtonSelector).click();
     cy.wait(3000);
 
-
+  
     // Simulate file upload
     cy.get('input[type="file"]').attachFile(fileName);
+  
     cy.wait(3000);
-
-
     // Click the "Upload" button
-    //   cy.get(uploadButtonSelector).click();
-
+  //   cy.get(uploadButtonSelector).click();
+  
     // Handle the final "OK" buttons
-    cy.get(okButtonSelector1).click();
-    cy.get(okButtonSelector2).click();
-
+    cy.xpath(okButtonSelector1).click(); 
+    cy.get(okButtonSelector2).click(); 
+  
     // Optional: Validate the upload (you can remove this if not needed)
-    //   cy.get('.upload-status').should('contain', 'Upload successful');
-});
+  //   cy.get('.upload-status').should('contain', 'Upload successful');
+  });
+
 Cypress.Commands.add('clickElementWithXpath', (selector) => {
     cy.xpath(selector).click({ force: true });
 });
